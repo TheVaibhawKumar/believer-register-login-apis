65fc234f1035744b52ad80f1fd93a389c87f7171
TheVaibhawKumar <codingvaibhaw@gmail.com>
2025-09-30 12:23:49 +0530

    upload files

--- src/loaders/security.ts
+++
@@ -1,8 +0,0 @@
-import helmet from 'helmet';
-import cors from 'cors';
-import { Express } from 'express';
-
-export const applySecurity = (app: Express) => {
-  app.use(helmet());
-  app.use(cors({ origin: true, credentials: true }));
-};
