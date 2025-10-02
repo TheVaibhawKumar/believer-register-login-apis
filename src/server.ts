79c922759c720316c927ed182b1c0e35bd0ba8cf
TheVaibhawKumar <codingvaibhaw@gmail.com>
2025-10-02 23:05:12 +0530

    remove comments

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,9 @@ import app from './app';
 import { connectDB } from './config/db';
 
 const PORT = process.env.PORT || 4000;
-
 const start = async () => {
 await connectDB();
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 };
-
 
 start();
