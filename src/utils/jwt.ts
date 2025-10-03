b9d70ff72832089363e978764ea2c8b5c2862a54
TheVaibhawKumar <codingvaibhaw@gmail.com>
2025-10-03 13:16:44 +0530

    update jwt code

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,7 +3,6 @@ import ms from "ms"; // comes with types for StringValue
 
 const SECRET: Secret = process.env.JWT_SECRET || "change_this_secret";
 const EXPIRES_IN: ms.StringValue = (process.env.JWT_EXPIRES_IN as ms.StringValue) || "1h";
-
 export const signJwt = (payload: object): string => {
   const options: SignOptions = { expiresIn: EXPIRES_IN };
   return jwt.sign(payload, SECRET, options);
