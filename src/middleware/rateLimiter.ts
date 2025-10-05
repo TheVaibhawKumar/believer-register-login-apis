e89efeddd54a7d26daa4acec73d94aed04ab2668
TheVaibhawKumar <codingvaibhaw@gmail.com>
2025-10-05 20:13:20 +0530

    update ratemeter file

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,7 +2,6 @@ import rateLimit from "express-rate-limit";
 
 const windowMs = (Number(process.env.RATE_LIMIT_WINDOW_MINUTES) || 15) * 60 * 1000;
 const max = Number(process.env.RATE_LIMIT_MAX_REQUESTS) || 100;
-
 export const apiLimiter = rateLimit({
   windowMs,
   max,
